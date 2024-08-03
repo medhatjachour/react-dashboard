cf567c6b1e4075ef0ed9c369d78a6f5fc5fa4840
medhatjachour <53466991+medhatjachour@users.noreply.github.com>
2024-08-03 16:40:01 +0300

    add calender
    list view is not working

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
+import Faq from "./scenes/faq";
 import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar/calendar";
 
@@ -35,7 +35,7 @@ function App() {
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
+              <Route path="/faq" element={<Faq />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} />
             </Routes>
--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
-const FAQ = () => {
+const Faq = () => {
   return (
-    <div>FAQ</div>
+    <div>Faq</div>
   )
 }
 
-export default FAQ
+export default Faq
